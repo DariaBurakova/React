2af76f91e56ef2cba0f54c5a848f7cd49fe52285
DariaBurakova <DariaBurakova@gmail.com>
2021-11-01 13:23:24 +0300

    lesson2

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,22 +1,56 @@
 
 import './App.css';
-import Message from "./components/Message";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {Form} from "./components/Form";
 
+const text=[
+    {text:'', author:''}
+];
 
+function App() {
+    const [messageList,setMessageList]=useState(text);
 
-function App({name}) {
-    const [text,setText]=useState('I am glad to see you');
-    const handleClick=()=>{
-       alert(text)
+    useEffect(()=>{
+      setTimeout(()=>{
+return setMessageList(messageList.map(item=>{
+    if(item.author==='user'){
+   return {
+       text: 'How are you?',
+       author: 'robot'
+   }
+    }else{
+        return{
+            text:'',
+            author: ''
+        }
     }
+})
+)},2000)
+  },[changeText])
+
+function changeText(value,author){
+    return setMessageList( messageList.map(item=>{
+        if(author==='user') {
+            return {
+                text: value,
+                author: author
+            }
+        }
+    })
+    )
+}
   return (
     <div className="App">
-      <header className="App-header">
-          <h3 className='heading'>Hello,my name is {name}</h3>
-          <Message onClick={handleClick}/>
+     <header >
       </header>
-
+        <main className="main-app">
+                <Form changeText={changeText}/>
+            {messageList.map((item)=>{
+               return (<div className="main-answer">
+                   {item.text} <br/>
+                   {item.author}
+            </div>)})}
+        </main>
     </div>
   );
 }
